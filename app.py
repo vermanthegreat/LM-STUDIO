d3ffebc879733bddb87460650de28902819b9e54
vermanthegreat <shecsar@gmail.com>
2026-06-17 18:47:21 +0300

    fix(ask): route web ask through SQLite query layer

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import db
-from ask_mcp import answer_question
 from extractor import parse_and_save
+from query import answer_question
 
 load_dotenv()
 
